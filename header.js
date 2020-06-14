81a695ea962ee2d978f86a5802b06c3bae025140
Rostik0123 <66382030+Rostik0123@users.noreply.github.com>
2020-06-14 12:47:37 +0300

    Add files via upload

--- header.js
+++ header.js
@@ -2,9 +2,9 @@ var headerText = `<div>
 <div class="block-menu">
   <img src="img/logo.png" alt="Gain" class="logo" href="">
   <a class="home text-menu" href="index.html">Главная</a>
-  <a class="kosmos text-menu" href="kosmos.html">Новости космоса</a>
-  <a class="earth text-menu" href="">Новости Земли</a>
-  <a class="project text-menu" href="">Новости проекта</a>
+  <a class="kosmos text-menu" href="kosmos-news.html">Новости космоса</a>
+  <a class="earth text-menu" href="Earth-news.html">Новости Земли</a>
+  <a class="project text-menu" href="project-news.html">Новости проекта</a>
 </div>
 <span></span>
 </div>`;
