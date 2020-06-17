c1a855e009ce57ab01d295ad9068363555ad3255
Rostik0123 <66382030+Rostik0123@users.noreply.github.com>
2020-06-17 17:38:01 +0300

    Add files via upload

--- header.js
+++ header.js
@@ -1,16 +1,16 @@
 var headerText = `<div>
 <div class="block-menu">
   <a href="index.html">
-    <img src="img/logo.png" alt="Gain" class="logo">
+    <img src="/Gain-main/img/logo.png" alt="Gain" class="logo">
   </a>
-  <a class="home text-menu" href="index.html">Главная</a>
-  <a class="kosmos text-menu" href="kosmos-news.html">Новости космоса</a>
-  <a class="earth text-menu" href="Earth-news.html">Новости Земли</a>
-  <a class="project text-menu" href="project-news.html">Новости проекта</a>
+  <a class="home text-menu" href="/Gain-main/index.html">Главная</a>
+  <a class="kosmos text-menu" href="/Gain-main/kosmos-news.html">Новости космоса</a>
+  <a class="earth text-menu" href="/Gain-main/Earth-news.html">Новости Земли</a>
+  <a class="project text-menu" href="/Gain-main/project-news.html">Новости редакции</a>
   <div class="dropdown">
     <a onclick="myFunction()" class="dropbtn">☰</a>
     <div id="myDropdown" class="dropdown-content">
-      <a href="support.html" class="support">Поддержка</a>
+      <a href="/Gain-main/support.html" class="support">Поддержка</a>
       <a href="#" class="about">О нас</a>
     </div>
   </div>
